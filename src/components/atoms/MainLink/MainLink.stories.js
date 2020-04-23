c5cf130b1dfcce668ae2a0dee617989e946c190c
Thomas Deruel <thomas.deruel@hetic.net>
2020-04-23 17:50:11 +0200

    [Front](add MainLink Component plus Storybook init)

--- src/components/atoms/MainLink/MainLink.stories.js
+++ src/components/atoms/MainLink/MainLink.stories.js
@@ -2,9 +2,7 @@ import React from "react";
 import MainLink from "./MainLink";
 
 export const Default = () => (
-  <MainLink size="small" onClick={onClickButton}>
-    Default
-  </MainLink>
+  <MainLink onClick={onClickButton}>Default</MainLink>
 );
 
 const onClickButton = (e) => {
