28ada82ca56f85f7d87c2cfabce20eb46b0d3ce5
ThomasFranja <thomas.franja@firmfunding.com>
2020-04-24 12:58:27 +0200

    [Front](update stories Title)

--- src/components/atoms/Title/Title.stories.js
+++ src/components/atoms/Title/Title.stories.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Title from './Title';
 
-export const TitleStandard = () => (
-  <Title title="Space jam"  color='#fff'/>
+export const TitleStandardLeft = () => (
+  <Title title="He Got the game" text="1996 - Warner Bros. Pictures"/>
+);
+
+export const TitleStandardRight = () => (
+  <Title title="Space jam" text="1996 - Warner Bros. Pictures" textAlign="right" DirectionBorderRight/>
 );
