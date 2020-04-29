aaa6c16a576240d30aa7760aabff2cb936e20b6c
ThomasFranja <thomas.franja@firmfunding.com>
2020-04-29 20:18:34 +0200

    [Front](Update stories components MainLink, styles + Update views Navigation)

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -1,27 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import DataContext from "../DataContext";
+import Main from '../components/layouts/Main';
+import {Default} from '../components/atoms/MainLink/MainLink.stories';
+import {Left, Right} from '../components/atoms/ButtonLink/ButtonLink.stories';
+import Cinemabg from '../assets/img/michael-jordan-space-jam.jpg';
 import '../assets/styles/reset.css';
 
 const Navigation = () => {
   return (
-    <DataContext.Consumer>
-      {(props) => (
-        <NavigationStyled>
-          <p>{props.data.content}</p>
-          <p>{props.data.loading}</p>
-          <p>Navigation</p>
-        </NavigationStyled>
-      )}
-    </DataContext.Consumer>
+    // <DataContext.Consumer>
+    //   {(props) => (
+    //     <NavigationStyled>
+    //       <p>{props.data.content}</p>
+    //       <p>{props.data.loading}</p>
+    //     </NavigationStyled>
+    //   )}
+    // </DataContext.Consumer>
+    <Main src={ Cinemabg }>
+      <div style={{ marginRight: '120px' }}>
+        <Left />
+      </div>
+      <Default> Marvel </Default>
+      <div style={{ marginLeft: '120px' }}>
+        <Right />
+      </div>
+    </Main>
   );
 };
 
 const NavigationStyled = styled.div`
-  color: red;
   background-color: #595959;
   width: 100%;
-  height: 
+  height: 100vh;
 `;
 
 export default Navigation;
